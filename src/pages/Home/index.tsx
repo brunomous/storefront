301d3beae0742cb1d1c82df750a15c1436b002ef
Jefferson Bruno <brnmouras@gmail.com>
2022-08-26 21:58:55 -0300

    feat: implement product queries

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,33 @@
+import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import { Link } from 'react-router-dom'
+
+import api from '../../api'
+
+import { Product as ProductType } from '../../types/Product'
+
 function Home(): JSX.Element {
+  const { isLoading, data } = useQuery(
+    ['products'],
+    async () => await api.get('/products'),
+  )
+
+  const products: ProductType[] = useMemo(() => {
+    return data?.data ?? []
+  }, [data])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      {products.map(({ id, title }, index: number) => (
+        <Link key={`product_${id}_${index}`} to={`/product/${id}`}>
+          {title}
+        </Link>
+      ))}
+    </div>
   )
 }
 
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,6 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import { useMemo } from 'react'
+
+import api from '../../api'
+
+import { Product as ProductType } from '../../types/Product'
+
 function Product(): JSX.Element {
+  const { id } = useParams()
+
+  const { isLoading, data } = useQuery(
+    ['product'],
+    async () => await api.get(`/products/${id}`),
+  )
+
+  const product: ProductType = useMemo(() => {
+    return data?.data
+  }, [data])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>Product</div>
+    <div>Product {id}: {product.title}</div>
   )
 }
 
